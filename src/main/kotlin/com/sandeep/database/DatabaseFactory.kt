d8a2f3c487e92b16ad6e60d5f86d22993d129454
msandeepk <90695071+mssandeepkamath@users.noreply.github.com>
2023-07-13 11:45:19 +0530

    Update DatabaseFactory.kt

--- src/main/kotlin/com/sandeep/database/DatabaseFactory.kt
+++ src/main/kotlin/com/sandeep/database/DatabaseFactory.kt
@@ -20,27 +20,45 @@ object DatabaseFactory
 
     }
 
-    private fun hikari() : HikariDataSource
-    {
+//     private fun hikari() : HikariDataSource
+//     {
 
-        val config=HikariConfig()
-        config.driverClassName = System.getenv("JDBC_DRIVER")
-//        config.jdbcUrl = System.getenv("DATABASE_URL")
-        config.maximumPoolSize = 3
-        config.isAutoCommit = false
-        config.transactionIsolation = "TRANSACTION_REPEATABLE_READ"
-        val uri= URI(System.getenv("DATABASE_URL"))
-        val username=uri.userInfo.split(":").toTypedArray()[0]
-        val password=uri.userInfo.split(":").toTypedArray()[1]
+//         val config=HikariConfig()
+//         config.driverClassName = System.getenv("JDBC_DRIVER")
+// //        config.jdbcUrl = System.getenv("DATABASE_URL")
+//         config.maximumPoolSize = 3
+//         config.isAutoCommit = false
+//         config.transactionIsolation = "TRANSACTION_REPEATABLE_READ"
+//         val uri= URI(System.getenv("DATABASE_URL"))
+//         val username=uri.userInfo.split(":").toTypedArray()[0]
+//         val password=uri.userInfo.split(":").toTypedArray()[1]
 
-        config.jdbcUrl="jdbc:postgresql://" + uri.host + ":" + uri.port + uri.path + "?sslmode=require" + "&user=$username&password=$password"
+//         config.jdbcUrl="jdbc:postgresql://" + uri.host + ":" + uri.port + uri.path + "?sslmode=require" + "&user=$username&password=$password"
 
 
-        config.validate()
+//         config.validate()
 
-        return HikariDataSource(config)
-    }
+//         return HikariDataSource(config)
+//     }
 
+private fun hikari(): HikariDataSource {
+    val config = HikariConfig()
+    config.driverClassName = "org.postgresql.Driver"
+    config.jdbcUrl = System.getenv("DATABASE_URL")
+    config.maximumPoolSize = 3
+    config.isAutoCommit = false
+    config.transactionIsolation = "TRANSACTION_REPEATABLE_READ"
+    
+    config.addDataSourceProperty("databaseName", System.getenv("PGDATABASE"))
+    config.addDataSourceProperty("serverName", System.getenv("PGHOST"))
+    config.addDataSourceProperty("portNumber", System.getenv("PGPORT"))
+    config.addDataSourceProperty("user", System.getenv("PGUSER"))
+    config.addDataSourceProperty("password", System.getenv("PGPASSWORD"))
+    
+    config.validate()
+
+    return HikariDataSource(config)
+}
     public suspend fun <T> dbQuery(block:()->T):T= withContext(Dispatchers.IO)
     {
         transaction {
